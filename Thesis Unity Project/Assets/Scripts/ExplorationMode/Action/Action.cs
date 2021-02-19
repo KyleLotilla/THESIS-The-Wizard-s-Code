ba087ae0618315b81008b051401ad91e34170105
marcomalabag <46800870+marcomalabag@users.noreply.github.com>
2021-02-19 12:50:06 +0800

    Added the default spells

---
+++ Thesis Unity Project/Assets/Scripts/ExplorationMode/Action/Action.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+abstract public class Action : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] public GameObject Wizard { get; set; }
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public abstract void Execute();
+    public abstract bool isFinishedExecuting();
+}
---
+++ Thesis Unity Project/Assets/Scripts/ExplorationMode/DefaultSpells/Movement/TPRight.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TPRight : Action
+{
+    bool isRunning;
+    WizardMovement wizardmovement;
+
+    public override void Execute()
+    {
+        this.wizardmovement = this.Wizard.GetComponent<WizardMovement>();
+
+        if (this.wizardmovement)
+        {
+            this.enabled = true;
+            this.isRunning = true; 
+            Vector2 destTile = this.Wizard.transform.position;
+            destTile.x += 0.5f;
+            this.wizardmovement.WalkToDestinationTile(destTile);
+        }
+    }
+
+    public override bool isFinishedExecuting()
+    {
+        return !this.isRunning;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.isRunning)
+        {
+            if (!(this.wizardmovement.shouldWalk))
+            {
+                this.isRunning = false;
+                this.enabled = false;
+            }
+        }
+    }
+}
