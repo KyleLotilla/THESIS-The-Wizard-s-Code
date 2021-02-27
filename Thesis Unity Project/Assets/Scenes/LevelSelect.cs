6f12114071d2893464048cd499eef8927fccd243
marcomalabag <46800870+marcomalabag@users.noreply.github.com>
2021-02-27 09:40:20 +0800

    Added XML loading for level select

---
+++ Thesis Unity Project/Assets/Scenes/LevelSelect.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Linq;
+using UnityEngine;
+
+public class LevelSelect : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        XElement LoadFile = XElement.Load("Assets/Scenes/XML/levels.xml");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
